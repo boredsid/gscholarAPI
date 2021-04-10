fa25ce0c951b5143e6c89bb6ab0de6c6a7b98681
Siddhant Narula <siddhant.narula96@gmail.com>
2021-04-10 14:53:12 +0530

    Added exception handling

--- gscholarFetcher.py
+++ gscholarFetcher.py
@@ -3,7 +3,10 @@ from scholarly import scholarly
 def get_author(author,university=""):
   url_part = "https://scholar.google.co.in/citations?user="
   authorSearch = scholarly.search_author(author+(", "+university if university!='' else ''))
-  authorResult = next(authorSearch)
+  try:
+    authorResult = next(authorSearch)
+  except:
+    return "Not Found"
   authorRaw = scholarly.fill(authorResult,sections=['basics','indices','publications'])
   authorDetails = {'name':authorRaw['name'],'affiliation':authorRaw['affiliation'],'email_domain':authorRaw['email_domain'],'interests':authorRaw['interests']
                   ,'publications':len(authorRaw['publications']),'citedby':authorRaw['citedby'],'hindex':authorRaw['hindex'],'i10index':authorRaw['i10index']
